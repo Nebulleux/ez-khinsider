d746830b03050b3f0c826d6213d8effab8fdd03d
Jean-Baptiste <42112354+Nebulleux@users.noreply.github.com>
2021-06-04 16:57:07 +0200

    Language update

--- khinsider-downloader.cmd
+++ khinsider-downloader.cmd
@@ -1,11 +1,12 @@
 @echo off
 
 echo Ce script a ete cree par le grand Nebulleux - Les OST viennent de [https://downloads.khinsider.com/]
+echo This script was created by the great Nebulleux - OST are from [https://downloads.khinsider.com/]
 
 pause
 
-set /p site="Entrez l'url de l'ost voulue svp : "
+set /p site="Put the website url here | Entrez l'url de l'ost voulue svp : "
 
-set /p format="Entrez le format voulu (mp3,m4a,flac): "
+set /p format="Put the desired format (mp3,m4a,flac)| Entrez le format voulu (mp3,m4a,flac): "
 
 khinsider.py --format %format% %site%
